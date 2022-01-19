32ee741b9d0597760cf0bea24fefc4a7cc85ec5f
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-01-19 11:17:33 +0000

    Rspec additional tests added for "fizzbuzz" to spec

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,28 @@
 require_relative '../lib/fizzbuzz'
+
+# RSpec.describe fizzbuzz, "number" do
+
+# describe 'multiples operation' do
+#    it 'returns zero if a multiple of specified number' do
+#        expect(number % multiple).to eq "0"
+#        number = '15'
+#        multiple = '3'
+
+
 describe 'fizzbuzz' do
-    it 'returns "fizz" when passed 3' do
-        expect(fizzbuzz(3)).to eq 'fizz'
-    end
+  it 'returns "fizz" when passed multiples of 3' do
+    expect(fizzbuzz(% 3 == 0)).to eq 'fizz'
+  end
 end
 
+describe 'fizzbuzz' do 
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when passed multiples of 3 or 5' do
+    expect(fizzbuzz(3 && 5)).to eq 'fizzbuzz'
+  end
+end
